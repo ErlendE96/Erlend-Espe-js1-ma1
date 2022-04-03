32ba80cff6700038347c2f4c056e080493e119b6
ErlendE96 <96021420+ErlendE96@users.noreply.github.com>
2022-04-03 21:31:52 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+//question 1
+var cat = {
+    complain: function() {
+        console.log("meow!");
+    }
+
+}
+
+//question 2
+const heading = document.querySelector("h3");
+    heading.innerHTML = "Updated heading";
+    class subheading {}
+
+//question 3
+heading.style.fontSize = "2em";
+
+//question 4
+
+const heading = document.querySelector("h3");
+    heading.innerHTML = "Updated heading";
+    class subheading {}
+
+//question 5
+
+const paragraphs = document.querySelectorAll("p");
+    for(var i = 0; i < paragraphs.length; i++) {
+    paragraphs.style.color = "red";
+    }
+
+//question 6
+const resultsContainer = document.querySelectorAll(".results");
+    resultsContainer.innerHTML = "<p>New paragraph</p>";
+    resultsContainer.body.style.backgroundcolor = "yellow";
+
